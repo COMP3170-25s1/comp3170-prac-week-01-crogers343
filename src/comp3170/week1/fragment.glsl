412b496d4c1fefae8d5de1191491b41268e2f468
ClaireRogers <claire.rogers@students.mq.edu.au>
2025-02-27 16:53:52 +1100

    added shaders and experimented with diamonds

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -6,10 +6,15 @@ uniform vec2 u_screenSize;  	// screen dimensions in pixels
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 p = gl_FragCoord.xy /  u_screenSize;   // scale p into range (0,0) to (1,1)
+  // float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+  vec2 v = abs(p - vec2(0.5, 0.5));
+  //float d = max(v.x, v.y);  
+  float d = v.y +v.x;   
   
-   if (d < 0.5) {
+   if (d < 0.2) { 
+   
+   // if distance to midpoint is less than x, paint red (creating a radius)
       o_colour = vec4(u_colour, 1);
    }
    else {
